b441973f8e15b8cfe49b9b63f4017b16df02d73a
marek2001 <marekmareckimm@interia.pl>
2025-01-28 18:47:55 +0100

    Przetłmaczono komentarze

--- CustomAdam.py
+++ CustomAdam.py
@@ -1,73 +1,79 @@
 import torch
 from torch.optim import Optimizer
 
-#Custom Adam Optimizer - extension of Optimizer class
-class CustomAdam(Optimizer):
-    
-    """
-    A custom implementation of the Adam optimizer. Defaults used are as recommended in https://arxiv.org/abs/1412.6980 
-    See the paper or visit Optimizer_Experimentation.ipynb for more information on how exactly Adam works + mathematics behind it.
 
-    Params:
-    stepsize (float): the effective upperbound of the optimizer step in most cases (size of step). DEFAULT - 0.001.
-    bias_m1 (float): bias for the first moment estimate. DEFAULT - 0.9
-    bias_m2 (float): bias for the second uncentered moment estimate, DEFAULT - 0.999.
-    epsilon (float): small number added to prevent division by zero, DEFAULT - 10e-8.
-    bias_correction (bool): whether the optimizer should correct for the specified biases when taking a step. DEFAULT - TRUE.
+# Niestandardowy optymalizator Adam - rozszerzenie klasy Optimizer
+class CustomAdam(Optimizer):
     """
-    #Initialize optimizer with parameters
-    def __init__(self, params, stepsize = 0.001, bias_m1 = 0.9, bias_m2 = 0.999, epsilon = 10e-8, bias_correction = True):
-        #Check if stepsizes and biases are invalid (negative)
+    Niestandardowa implementacja optymalizatora Adam. Domyślne wartości są zgodne z zaleceniami zawartymi w https://arxiv.org/abs/1412.6980.
+    Zobacz publikację lub plik Optimizer_Experimentation.ipynb, aby dowiedzieć się więcej o działaniu Adama oraz jego matematycznych podstawach.
+
+    Parametry:
+    stepsize (float): efektywna górna granica kroku optymalizatora (rozmiar kroku). DOMYŚLNIE - 0.001.
+    bias_m1 (float): współczynnik dla pierwszego momentu. DOMYŚLNIE - 0.9
+    bias_m2 (float): współczynnik dla drugiego, niecentrowanego momentu. DOMYŚLNIE - 0.999.
+    epsilon (float): mała wartość dodawana w celu zapobieżenia dzieleniu przez zero. DOMYŚLNIE - 10e-8.
+    bias_correction (bool): określa, czy optymalizator powinien korygować określone biasy podczas wykonywania kroku. DOMYŚLNIE - TRUE.
+    """
+
+    # Inicjalizacja optymalizatora z podanymi parametrami
+    def __init__(self, params, stepsize=0.001, bias_m1=0.9, bias_m2=0.999, epsilon=10e-8, bias_correction=True):
+        # Sprawdzenie, czy współczynnik kroku oraz wartości biasów są prawidłowe (czy nie są ujemne)
         if stepsize < 0:
-            raise ValueError("Invalid stepsize [{}]. Choose a positive stepsize".format(stepsize))
+            raise ValueError("Nieprawidłowy współczynnik kroku [{}]. Wybierz dodatnią wartość.".format(stepsize))
         if bias_m1 < 0 or bias_m2 < 0 and bias_correction:
-            raise ValueError("Invalid bias parameters [{}, {}]. Choose positive bias parameters.".format(bias_m1, bias_m2))
-        #Declare dictionary of default values for optimizer initialization
-        DEFAULTS = dict(stepsize = stepsize, bias_m1 = bias_m1, bias_m2 = bias_m2, epsilon = epsilon, bias_correction = bias_correction)
-        #Initialize the optimizer
+            raise ValueError(
+                "Nieprawidłowe parametry biasu [{}, {}]. Wybierz dodatnie wartości.".format(bias_m1, bias_m2))
+        # Deklaracja słownika domyślnych wartości dla optymalizatora
+        DEFAULTS = dict(stepsize=stepsize, bias_m1=bias_m1, bias_m2=bias_m2, epsilon=epsilon,
+                        bias_correction=bias_correction)
+        # Inicjalizacja optymalizatora
         super(CustomAdam, self).__init__(params, DEFAULTS)
 
-    #Step method (for updating parameters)
-    def step(self, closure = None):
-        #Set loss to none
+    # Metoda wykonująca krok optymalizacji (aktualizację parametrów)
+    def step(self, closure=None):
+        # Ustawienie wartości straty na None
         loss = None
-        #If the closure is set to True, set the loss to the closure function
+        # Jeśli podano closure, przypisanie wartości straty do wyniku closure
         loss = closure() if closure != None else loss
-        #Check if this is the first step - if not, increment the current step
+        # Sprawdzenie, czy jest to pierwszy krok - jeśli tak, ustawienie wartości na 1, w przeciwnym razie inkrementacja
         if not self.state["step"]:
             self.state["step"] = 1
         else:
             self.state["step"] += 1
-        #Iterate over "groups" of parameters (layers of parameters in the network) to begin processing and computing the next set of params
+        # Iteracja po grupach parametrów (warstwach sieci neuronowej) w celu rozpoczęcia obliczeń i aktualizacji parametrów
         for param_group in self.param_groups:
-            #Iterate over individual parameters
+            # Iteracja po poszczególnych parametrach
             for param in param_group["params"]:
-                #Check if gradients have been computed for each parameter
-                #If not - if there are no gradients - then skip the parameter
+                # Sprawdzenie, czy dla danego parametru obliczono gradient
+                # Jeśli gradienty nie są dostępne - pomijamy ten parametr
                 if param.grad.data == None:
                     continue
-                else: gradients = param.grad.data
-                #Use Adam optimization method - first, define all the required arguments for the parameter if we are on the first step
+                else:
+                    gradients = param.grad.data
+                # Zastosowanie metody Adam - na pierwszym kroku inicjalizacja wymaganych wartości dla danego parametru
                 if self.state["step"] == 1:
-                    #Set the first and second moment estimates to zeroes
+                    # Ustawienie pierwszego i drugiego momentu na wektory zerowe
                     self.state["first_moment_estimate"] = torch.zeros_like(param.data)
                     self.state["second_moment_estimate"] = torch.zeros_like(param.data)
-                #Declare variables from state - inplace methods modify state variable directly
+                # Deklaracja zmiennych na podstawie wartości w stanie optymalizatora (modyfikacje są wykonywane in-place)
                 first_moment_estimate = self.state["first_moment_estimate"]
                 second_moment_estimate = self.state["second_moment_estimate"]
-                #Compute the first moment estimate - B_1 * m_t + (1-B_1) * grad (uncentered)
+                # Obliczenie pierwszego momentu - B_1 * m_t + (1-B_1) * grad (niecentrowany)
                 first_moment_estimate.mul_(param_group["bias_m1"]).add_(gradients * (1.0 - param_group["bias_m1"]))
-                #Compute the second moment estimate - B_2 * v_t + (1-B_2) * grad^2 (uncentered)
-                second_moment_estimate.mul_(param_group["bias_m2"]).add_(gradients.pow_(2) * (1.0 - param_group["bias_m2"]))
-                #Perform bias correction if parameter is set to true
+                # Obliczenie drugiego momentu - B_2 * v_t + (1-B_2) * grad^2 (niecentrowany)
+                second_moment_estimate.mul_(param_group["bias_m2"]).add_(
+                    gradients.pow_(2) * (1.0 - param_group["bias_m2"]))
+                # Wykonanie korekty biasu, jeśli parametr jest ustawiony na True
                 if param_group["bias_correction"]:
-                    #Perform bias correction for the first moment estimate: m_t / (1 -B_1^t)
+                    # Korekta biasu dla pierwszego momentu: m_t / (1 - B_1^t)
                     first_moment_estimate.divide_(1.0 - (param_group["bias_m1"] ** self.state["step"]))
-                    #Perform bias correction for second moment estimate: v_t / (1 - B_2^t)
+                    # Korekta biasu dla drugiego momentu: v_t / (1 - B_2^t)
                     second_moment_estimate.divide_(1.0 - (param_group["bias_m2"] ** self.state["step"]))
-                #Next, perform the actual update
-                #Multiply the stepsize a by the quotient of the first moment estimate and the square root of the second moment estimate plus epsilon
-                #In other words - theta = theta_{t-1} - a * first_estimate/(sqr(second_estimate) + epsilon)
-                param.data.add_((-param_group["stepsize"]) * first_moment_estimate.divide_(second_moment_estimate.sqrt_() + param_group["epsilon"]))
-        #Return the loss
+                # Następnie wykonujemy właściwą aktualizację parametru
+                # Przemnażamy współczynnik kroku (stepsize) przez iloraz pierwszego momentu i pierwiastka drugiego momentu powiększonego o epsilon
+                # Wzór: theta = theta_{t-1} - stepsize * first_estimate / (sqrt(second_estimate) + epsilon)
+                param.data.add_((-param_group["stepsize"]) * first_moment_estimate.divide_(
+                    second_moment_estimate.sqrt_() + param_group["epsilon"]))
+        # Zwrócenie wartości straty
         return loss
